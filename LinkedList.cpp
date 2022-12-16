388bc637e28d71e128a9b45d05bc353c87972a89
nicole-jefferies-uoa <nicol@DESKTOP-MVQ8NMM>
2022-12-16 21:37:28 +1030

    add

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -4,22 +4,78 @@ struct node
 {
     int data;
     node *next_node;
-    node *previous_node;
 };
 
 class linked_list
 {
-    private:
-        node *head_of_list, *tail_of_list;
-    public:
-        linked_list(){
-            head_of_list = NULL;
-            tail_of_list = NULL;
+
+public:
+    node *listHead, *listTail;
+
+    linked_list()
+    {
+        listHead = NULL;
+        listTail = NULL;
+    }
+
+    void add_node(int data)
+    {
+        node *newNode = new node;
+        newNode->data = data;
+        newNode->next_node = NULL;
+
+        if (listHead == NULL) // if list is empty
+        {
+            listHead = newNode;
+            listTail = newNode;
+        }
+        else
+        {
+            listTail->next_node = newNode;
+            listTail = newNode;
+        }
+    }
+
+    node *search_list(int data)
+    {
+        node *currentNode = listHead;
+        while (currentNode->next_node != NULL)
+        {
+            if (currentNode->data == data)
+            {
+                std::cout << data << " found at: " << currentNode << std::endl;
+                return currentNode;
+            }
+
+            currentNode = currentNode->next_node;
         }
 
-        void insert_node_to_beginning_of_list(int node){
-            
-        }
+        std::cout << data << " not found in linked list" << std::endl;
+        return NULL;
+    }
+
+    void delete_node(int data){
+        if(listHead == NULL)
+            return;
+
+        if(listHead->data == data)
+            listHead = listHead->next_node;
+
+        if(search_list == NULL)
+            std:cout << 
+    }
 
 };
 
+int main()
+{
+    linked_list list;
+
+    list.add_node(7);
+    list.add_node(22);
+
+    list.search_list(7);
+    list.search_list(17);
+
+    return 0;
+}
