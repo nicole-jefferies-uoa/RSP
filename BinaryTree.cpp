98a0bbbd2442fce17dff8e7d92a35536d76a83de
nicole-jefferies-uoa <nicol@DESKTOP-MVQ8NMM>
2022-12-17 16:50:49 +1030

    add

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -3,7 +3,6 @@
 struct node
 {
     int data;
-    // node *parent;
     node *right_child;
     node *left_child;
 };
@@ -11,6 +10,18 @@ struct node
 class binary_tree
 {
 public:
+    node *rootNode;
+
+    binary_tree(){}
+
+    binary_tree(int data){
+        rootNode = create_node(data);
+    }
+
+    node *create_root_node(int data){
+        rootNode = create_node(data);
+    }
+
     node *create_node(int data)
     {
         node *newNode = new node;
@@ -21,17 +32,26 @@ public:
         return newNode;
     }
 
-    node *insert_new_node(node *rootNode, int data)
+    node *insert_new_node(int data)
+    {
+        insert(rootNode, data);
+    }
+
+    node *insert(node *rootNode, int data)
     {
         if (rootNode == NULL)
             return create_node(data);
 
         else if (data > rootNode->data)
-            rootNode->right_child = insert_new_node(rootNode->right_child, data);
+            rootNode->right_child = insert(rootNode->right_child, data);
         else
-            rootNode->left_child = insert_new_node(rootNode->left_child, data);
+            rootNode->left_child = insert(rootNode->left_child, data);
 
         return rootNode;
+    }
+
+    node *search_tree(int data){
+        search_tree(rootNode, data);
     }
 
     node *search_tree(node *rootNode, int data)
@@ -44,9 +64,14 @@ public:
             return search_tree(rootNode->left_child, data);
     }
 
+    
+    node *find_minimum(){
+        find_minimum(rootNode);
+    }
+    
     node *find_minimum(node *rootNode)
     {
-        if (rootNode == NULL)
+        if (treeEmpty(rootNode))
             return NULL;
 
         else if (rootNode->left_child != NULL)
@@ -54,14 +79,91 @@ public:
 
         return rootNode;
     }
+
+    void printInOrder(){
+        std::cout << "Printing BST:" << std::endl;
+        printInOrder(rootNode);
+    }
+    
+    void printInOrder(node *rootNode)
+    {
+        if (!treeEmpty(rootNode))
+        {
+            std::cout << rootNode->data << std::endl;
+            printInOrder(rootNode->left_child);
+            printInOrder(rootNode->right_child);
+        }
+    }
+
+    node *delete_node(int data){
+        delete_node(rootNode, data);
+    }
+
+    node *delete_node(node *rootNode, int data)
+    {
+        if (treeEmpty(rootNode))
+            return NULL;
+
+        if (data > rootNode->data)
+            rootNode->right_child = delete_node(rootNode->right_child, data);
+
+        else if (data < rootNode->data)
+            rootNode->left_child - delete_node(rootNode->left_child, data);
+
+        else
+        {
+            if (rootNode->left_child == NULL && rootNode->right_child == NULL)
+            {
+                delete (rootNode);
+                this->rootNode = NULL;
+                return NULL;
+            }
+
+            else if (rootNode->left_child == NULL || rootNode->right_child == NULL)
+            {
+                node *temp;
+                if (rootNode->left_child == NULL)
+                    temp = rootNode->right_child;
+                else
+                    temp = rootNode->left_child;
+                delete(rootNode);
+                this->rootNode = temp;
+                return temp;
+            }
+
+            else
+            {
+                node *temp = find_minimum(rootNode->right_child);
+                rootNode->data = temp->data;
+                rootNode->right_child = delete_node(rootNode->right_child,temp->data); 
+            }
+        }
+    }
+
+    bool treeEmpty(node *rootNode)
+    {
+        if (rootNode == NULL)
+            return true;
+        return false;
+    }
+
 };
 
-int main(){
-    binary_tree *tree = new binary_tree;
+int main()
+{
+    binary_tree *tree = new binary_tree(12);
 
-    node *root = tree->create_node(26);
+    tree->insert_new_node(26);
 
-    tree->insert_new_node();
+    tree->printInOrder();
+
+    tree->insert_new_node(34);
+
+    tree->printInOrder();
+
+    tree->delete_node(26);
+
+    tree->printInOrder();
 
     return 0;
 }
